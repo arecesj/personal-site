d1015a70e6c399c171e27c94bb6346976e753d02
JUAN ARECES <arecesj@gmail.com>
2019-01-02 16:49:09 -0800

    finished home and about pages

--- src/AboutContainer.js
+++ src/AboutContainer.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react';
-import About from './About';
-import Contact from './Contact';
+import { Link } from 'react-router-dom';
+import './AboutContainer.css';
 class AboutContainer extends Component {
   render() {
     return (
-      <div>
-        <h2>About</h2>
-        <About />
-        <Contact />
+      <div className="about-container fade-in">
+        {/* About Text */}
+        <div className="about-wrapper">
+          <div className="about-text">
+            {/* About Text Title */}
+            <div className="about-text-title">
+              <h1>Hey There</h1>
+            </div>
+            {/* About Text Body */}
+            <div className="about-text-body">
+              <p>
+                I am a software developer living in San Francisco that has a lot
+                of love for problem solving in smart, beautiful, and efficient
+                ways. Check out my{' '}
+                <Link style={{ color: 'black' }} to="/projects">
+                  current projects
+                </Link>{' '}
+                for things that interest me.
+              </p>
+            </div>
+          </div>
+          {/* About Contact */}
+          <div className="about-contact">
+            <div className="about-contact-title">
+              <h1>Let's Chat</h1>
+            </div>
+            <div className="about-contact-body">
+              <p>
+                {' '}
+                I enjoy receiving and responding to emails. If you want to chat,
+                then so do I. Email me at{' '}
+                <a style={{ color: 'black' }} href="mailto:jdareces@gmail.com">
+                  jdareces@gmail.com
+                </a>
+                .
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
